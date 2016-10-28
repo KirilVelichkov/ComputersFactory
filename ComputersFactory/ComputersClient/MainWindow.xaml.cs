fd51dd3e5674c13cabd89f3e11c55905d657f058
Kalito98 <Kaloyan.Yanev98@gmail.com>
2016-10-28 17:00:17 +0300

    Adding again Client after forgetting some files

---
+++ ComputersFactory/ComputersClient/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using ComputersClient.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComputersClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            var addVideoCardWindow = new addVideoCard();
+            addVideoCardWindow.Show();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var addMemoryWindow = new addMemory();
+            addMemoryWindow.Show();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var addProcesorWindow = new addProcesor();
+            addProcesorWindow.Show();
+        }
+    }
+}
---
+++ ComputersFactory/ComputersClient/Windows/addMemory.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ComputersClient.Windows
+{
+    /// <summary>
+    /// Interaction logic for addMemory.xaml
+    /// </summary>
+    public partial class addMemory : Window
+    {
+        public addMemory()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
